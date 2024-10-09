794f2b1ead5b60b1225c537dcec5904a3ba9881c
Fr3zy <fr3zydev@gmail.com>
2024-10-10 02:18:13 +0300

    Local fonts are deleted and fonts are taken directly from the module.

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,18 +1,8 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/Providers/Theme";
 import AuthProvider from "@/components/Providers/Auth";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Toaster } from "sonner";
+import { GeistSans } from "geist/font/sans";
 
 export const metadata = {
   title: "LoopCord #SOON",
@@ -22,11 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${GeistSans.className} antialiased`}>
         <AuthProvider>
           <ThemeProvider attribute="class" defaultTheme="dark">
+            <Toaster
+              toastOptions={{
+                className: "bg-accent text-foreground border-2 border-border",
+              }}
+            />
             {children}
           </ThemeProvider>
         </AuthProvider>
--- client/app/page.js
+++ client/app/page.js
@@ -1,18 +1,19 @@
-"use client";
-const { useAuthStore } = require("@/stores/user");
+import { BorderBeam } from "@/components/ui/border-beam";
 
-export default function ProfileComponent() {
-  const { user, isLoading, error } = useAuthStore();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-  if (!user) return <div>Please log in</div>;
-
-  console.log(user);
-
+export default function HomeComponent() {
   return (
-    <div>
-      <h1>Welcome, {user.username}</h1>
+    <div className="p-24">
+      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+          LoopCord #SOON
+        </span>
+        <BorderBeam
+          colorFrom="#ed0518"
+          colorTo="#0de0d9"
+          size={250}
+          duration={12}
+        />
+      </div>
     </div>
   );
 }
