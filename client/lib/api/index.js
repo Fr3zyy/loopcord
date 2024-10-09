2c4804783dc6c4cfe88b403d89124965d15ab0b2
Fr3zy <fr3zydev@gmail.com>
2024-10-09 22:14:29 +0300

    Added client side.

---
+++ client/lib/api/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true
+});
+
+export default api;
